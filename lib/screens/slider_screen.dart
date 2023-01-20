5b00e690efdd49c71fd82bdcaa33edd9d9857fd3
Satasiya Chirag <satasiyachirag314@gmail.com>
2023-01-20 20:53:27 +0530

    chirag{slider add}

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class NewScreeen extends StatefulWidget {
@@ -31,20 +32,19 @@ class _NewScreeenState extends State<NewScreeen> {
 
   @override
   Widget build(BuildContext context) {
-
     Size size = MediaQuery.of(context).size;
     double height = size.height;
     double width = size.width;
     double text = MediaQuery.of(context).textScaleFactor;
 
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-            children: [
-              CarouselSlider(
-                items: sliderList
-                    .map(
-                      (item) => Container(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          CarouselSlider(
+            items: sliderList
+                .map(
+                  (item) => Container(
                     color: Colors.black,
                     height: height * .55,
                     child: Column(
@@ -63,7 +63,8 @@ class _NewScreeenState extends State<NewScreeen> {
                             text: sliderList[index]["title"],
                             style: TextStyle(
                               fontSize: text * 26,
-                              fontFamily: 'IntegralCF',),
+                              fontFamily: 'IntegralCF',
+                            ),
                           ),
                         ),
                         RichText(
@@ -71,7 +72,8 @@ class _NewScreeenState extends State<NewScreeen> {
                             text: sliderList[index]["subtitle"],
                             style: TextStyle(
                               fontSize: text * 26,
-                              fontFamily: 'IntegralCF',),
+                              fontFamily: 'IntegralCF',
+                            ),
                           ),
                         ),
                         const SizedBox(height: 10),
@@ -79,42 +81,44 @@ class _NewScreeenState extends State<NewScreeen> {
                     ),
                   ),
                 )
-                    .toList(),
-                carouselController: carouselController,
-                options: CarouselOptions(
-                  autoPlay: false,
-                  enlargeCenterPage: false,
-                  height: height * 0.75,
-                  viewportFraction: 1,
-                  onPageChanged: (index, reason) {
-                    this.index = index;
-                    setState(() {});
-                  },
-                ),
-              ),
-              SizedBox(height: height/8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: sliderList.asMap().entries.map((entry) {
-                  return GestureDetector(
-                    onTap: () => carouselController.animateToPage(entry.key),
-                    child: Container(
-                      width: 30.0,
-                      height: 8.0,
-                      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        //shape: BoxShape.circle,
-                        color: (Colors.white).withOpacity(
-                          index == entry.key ? 0.9 : 0.4,
-                        ),
+                .toList(),
+            carouselController: carouselController,
+            options: CarouselOptions(
+              autoPlay: false,
+              enlargeCenterPage: false,
+              height: height * 0.75,
+              viewportFraction: 1,
+              onPageChanged: (index, reason) {
+                this.index = index;
+                setState(() {});
+              },
+            ),
+          ),
+          SizedBox(height: height / 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: sliderList.asMap().entries.map(
+              (entry) {
+                return GestureDetector(
+                  onTap: () => carouselController.animateToPage(entry.key),
+                  child: Container(
+                    width: 30.0,
+                    height: 8.0,
+                    margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      //shape: BoxShape.circle,
+                      color: (Colors.white).withOpacity(
+                        index == entry.key ? 0.9 : 0.4,
                       ),
                     ),
-                  );
-                },).toList(),
-              ),
-            ],
-            ),
-        );
-    }
-
+                  ),
+                );
+              },
+            ).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
