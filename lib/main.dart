ca2cb82bbdc42e402ddceba888f4f76da9e797bf
mohitasoda <mohitasodariya07@gmail.com>
2023-01-29 20:09:44 +0530

    Mohit (screen name change)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gofit/screens/slider_screen.dart';
+import 'package:gofit/Widget/slider_progress_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: NewScreeen(),
+      home: const SliderProgressScreen(),
     );
   }
 }
