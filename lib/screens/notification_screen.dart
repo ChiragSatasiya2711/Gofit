1bdfcf738d9c78dc6bc3f6d31d63b8a700529fa1
Satasiya Chirag <satasiyachirag314@gmail.com>
2023-02-22 17:00:10 +0530

    chirag{screen make}

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -11,13 +11,16 @@ class NotificationScreen extends StatefulWidget {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
-  bool status1 = false;
-  bool status2 = false;
-  bool status3 = false;
-  bool status4 = false;
-  bool status5 = false;
-  bool status6 = false;
-
+  Map data = {
+    "data": [
+      {"data1": "General Notification", "data2": false},
+      {"data1": "Sound", "data2": false},
+      {"data1": "Vibrate", "data2": false},
+      {"data1": "App Updates", "data2": false},
+      {"data1": "New Service Available", "data2": false},
+      {"data1": "New tips available", "data2": false},
+    ],
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -25,189 +28,49 @@ class _NotificationScreenState extends State<NotificationScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            const SizedBox(
-              height: 20,
-            ),
             Row(
-              children: const [
-                BackButton(),
+              children: [
+                BackButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
                 Text(
                   "Notification",
                   style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20, left: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      const Text(
-                        "General Notification",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
+            ListView.builder(
+              physics: BouncingScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: data["data"].length,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      data["data"][index]["data1"],
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
                       ),
-                      const SizedBox(
-                        width: 165,
-                      ),
-                      FlutterSwitch(
-                        value: status1,
-                        height: 25,
-                        width: 50,
-                        onToggle: (val) {
-                          setState(() {
-                            status1 = val;
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    children: [
-                      const Text(
-                        "Sound",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 265,
-                      ),
-                      FlutterSwitch(
-                        value: status2,
-                        height: 25,
-                        width: 50,
-                        onToggle: (val) {
-                          setState(() {
-                            status2 = val;
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    children: [
-                      const Text(
-                        "Vibrate",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 260,
-                      ),
-                      FlutterSwitch(
-                        value: status3,
-                        height: 25,
-                        width: 50,
-                        onToggle: (val) {
-                          setState(() {
-                            status3 = val;
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    children: [
-                      const Text(
-                        "App Updates",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 220,
-                      ),
-                      FlutterSwitch(
-                        value: status4,
-                        height: 25,
-                        width: 50,
-                        onToggle: (val) {
-                          setState(() {
-                            status4 = val;
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    children: [
-                      const Text(
-                        "New Service Available",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 150,
-                      ),
-                      FlutterSwitch(
-                        value: status5,
-                        height: 25,
-                        width: 50,
-                        onToggle: (val) {
-                          setState(() {
-                            status5 = val;
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    children: [
-                      const Text(
-                        "New tips available",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 180,
-                      ),
-                      FlutterSwitch(
-                        value: status6,
-                        height: 25,
-                        width: 50,
-                        onToggle: (val) {
-                          setState(() {
-                            status6 = val;
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                ],
+                    ),
+                    FlutterSwitch(
+                      value: data["data"][index]["data2"],
+                      height: 25,
+                      width: 50,
+                      onToggle: (val) {
+                        setState(() {
+                          data["data"][index]["data2"] = val;
+                        });
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
